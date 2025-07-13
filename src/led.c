d8462902c03d7337a70ec1ed07c55db45be49a5b
Derek Maeshiro <derekmaeshiro@gmail.com>
2025-07-12 17:00:53 -0700

    committing format changes

--- src/led.c
+++ src/led.c
@@ -4,12 +4,12 @@
 
 #define LED_PIN 5
 
-void led_init(void) 
+void led_init(void)
 {
-    GPIOA->MODER |= (1U << (LED_PIN * 2));     // Set PB0 to output mode (01)
+    GPIOA->MODER |= (1U << (LED_PIN * 2)); // Set PB0 to output mode (01)
 }
 
 void led_toggle(void)
 {
-    GPIOA->ODR ^= (1U << LED_PIN); 	     // Toggle PB0
+    GPIOA->ODR ^= (1U << LED_PIN); // Toggle PB0
 }
--- src/main.c
+++ src/main.c
@@ -6,16 +6,17 @@
 
 int main(void)
 {
-	// Enable the GPIOB clock
-	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN; // Enable clock for GPIO port B                  
-	// Set pin PB0 as output
-	GPIOA->MODER &= ~(3U << (LED_PIN * 2));    // Clear mode bits for PB0
-	led_init();
-	// GPIOA->MODER |= (1U << (LED_PIN * 2));
-		     
-	while (1) {
-		led_toggle();
-		// GPIOA->ODR ^= (1U << LED_PIN);
-		for (volatile int i = 0; i < 100000; i++); // Busy wait
-	}
+    // Enable the GPIOB clock
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN; // Enable clock for GPIO port B
+    // Set pin PB0 as output
+    GPIOA->MODER &= ~(3U << (LED_PIN * 2)); // Clear mode bits for PB0
+    led_init();
+    // GPIOA->MODER |= (1U << (LED_PIN * 2));
+
+    while (1) {
+        led_toggle();
+        // GPIOA->ODR ^= (1U << LED_PIN);
+        for (volatile int i = 0; i < 100000; i++)
+            ; // Busy wait
+    }
 }
