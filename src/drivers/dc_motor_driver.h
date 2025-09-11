7857c8a6f0c7402f5f0955ffd1129b1ce98078c6
Derek Maeshiro <derekmaeshiro14@gmail.com>
2025-09-11 02:26:28 -0700

    fixing compile errors

--- src/drivers/dc_motor_driver.h
+++ src/drivers/dc_motor_driver.h
@@ -2,25 +2,8 @@
 #define DC_MOTOR_DRIVER_H
 
 #include <stdint.h>
-#include "../common/joints.h"
 
 #define NUM_DC_MOTOR_JOINTS 5
-
-typedef enum {
-    PWM_L_WRIST_NAE_NAE = 0,
-    PWM_L_ELBOW,
-    PWM_L_BICEP,
-    PWM_L_SHOULDER_FRONT_RAISE,
-    PWM_L_SHOULDER_LAT_RAISE,
-
-    PWM_R_WRIST_NAE_NAE,
-    PWM_R_ELBOW,
-    PWM_R_BICEP,
-    PWM_R_SHOULDER_FRONT_RAISE,
-    PWM_R_SHOULDER_LAT_RAISE,
-
-    PWM_CHANNEL_COUNT
-} pwm_channel_e;
 
 void smart_motor_init(void);
 void smart_motor_set_angle(joint_e joint, uint16_t angle);
